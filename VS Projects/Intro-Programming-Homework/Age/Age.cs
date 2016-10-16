788f576b234d47e49965318956b02af42e84154c
Zdronch <kristian.danchev@gmail.com>
2016-10-17 02:29:59 +0300

    Introduction homework completion (only item 16 is to be completed)

---
+++ VS Projects/Intro-Programming-Homework/Age/Age.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Age
+{
+    class Age
+    {
+        static void Main()
+        {
+            String MyBD = Console.ReadLine();
+            DateTime dt = Convert.ToDateTime(MyBD);
+            DateTime Today = DateTime.Now;
+            TimeSpan span = Today.Subtract(dt);
+            int years = (int)(span.Days / 365.25);
+            int nty = 10;
+            Console.WriteLine(years);
+            Console.WriteLine(years + nty);
+        }
+    }
+}
