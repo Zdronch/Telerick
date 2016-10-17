02cb19a746152520b7fe2315aea77c82cd64c97e
Zdronch <kristian.danchev@gmail.com>
2016-10-18 00:43:53 +0300

    Completed project PrintLongSequence

---
+++ VS Projects/Intro-Programming-Homework/PrintLongSequence/PrintLongSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintLongSequence
+{
+    class PrintLongSequence
+    {
+        static void Main()
+        {
+            int count = 2;
+            for (int i = count; i <= 1001; i++)
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else if (i % 2 != 0)
+
+                {
+                    Console.WriteLine("-" + i);
+                }
+        }
+    }
+}
